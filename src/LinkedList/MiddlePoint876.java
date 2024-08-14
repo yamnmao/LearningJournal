b98d10bd23ca736d40133b7aef54f1727d5d17d6
Yann/Yaming <57467587+yamnmao@users.noreply.github.com>
2024-08-14 18:17:39 +0800

    Create MiddlePoint876.java

---
+++ src/LinkedList/MiddlePoint876.java
@@ -0,0 +1,20 @@
+package LinkedList;
+
+public class MiddlePoint876 {
+    public class ListNode{
+        int val;
+       ListNode next;
+        ListNode(){}
+        ListNode(int val){this.val = val;}
+        ListNode(int val, ListNode next){this.val=val;this.next=next;}
+    }
+
+    public ListNode middleNode(ListNode head){
+        ListNode slow = head, fast = head;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
